25901b36bdad5889d35ace8fe57d45e834bcdb3a
Lukas Fryc <lukas@fryc.eu>
2015-03-20 15:56:23 +0100

    removed unused fhheader directove FHTEMPLATE-8

--- www/app/directives.js
+++ www/app/directives.js
@@ -2,22 +2,6 @@
 
 var myApp = angular.module('myApp.directives', []);
 
-myApp.directive('fhheader', function() {
-    return {
-        scope: {},
-        restrict: 'E',
-        replace: true,
-        templateUrl: 'views/components/header.html',
-        link: function(scope, elem, attrs, ctrl) {
-            var headerProps = {
-                title: attrs.title,
-                subtext: attrs.subtext
-            };
-            scope.title = headerProps;
-        }
-    };
-});
-
 myApp.directive('fhfooter', function() {
     return {
         scope: {},
